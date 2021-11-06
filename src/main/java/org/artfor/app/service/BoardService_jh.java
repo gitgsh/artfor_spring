556768de550d15030954ba6491d589046f8dba04
myk <koreash321@gmail.com>
2021-11-06 18:45:58 +0900

    1106_1845_이클립스 디테일페이지 분할

---
+++ src/main/java/org/artfor/app/service/BoardService_jh.java
@@ -0,0 +1,15 @@
+package org.artfor.app.service;
+
+import java.util.List;
+
+import org.artfor.app.web.BoardDTO;
+
+public interface BoardService_jh {
+
+	public List<BoardDTO> list();
+
+	public void input(BoardDTO dto);
+
+	
+
+}
